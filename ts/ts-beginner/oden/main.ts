a4bf37e35f2d627fbbd99f232e149466164ff122
Kamenleon <leon.kunishi@gmail.com>
2025-05-06 20:28:54 +0900

    add: Section08-02 done

---
+++ ts/ts-beginner/oden/main.ts
@@ -0,0 +1,11 @@
+async function handler() {
+  const html = await Deno.readTextFile("./index.html");
+  const response = new Response(html, {
+    headers: {
+      "content-type": "text/html;charset=utf-8",
+    },
+  });
+  return response;
+}
+
+Deno.serve(handler);
