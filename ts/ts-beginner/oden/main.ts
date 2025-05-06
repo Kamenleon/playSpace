383421e7597db45cbda2c891252ad6402bff5bc2
Kamenleon <leon.kunishi@gmail.com>
2025-05-06 21:21:42 +0900

    #2 mod: OpenAIがクレジット切れだったので8章は読むだけ

--- ts/ts-beginner/oden/main.ts
+++ ts/ts-beginner/oden/main.ts
@@ -1,10 +1,26 @@
+import OpenAI from "https://deno.land/x/openai@v4.62.1/mod.ts";
+
+const openai = new OpenAI({
+  apiKey: Deno.env.get("OPENAI_API_KEY"),
+});
+
 async function handler() {
-  const html = await Deno.readTextFile("./index.html");
-  const response = new Response(html, {
+  const completion = await openai.chat.completions.create({
+    messages: [
+      { role: "system", content: "レシピ作成アシスタント" },
+      { role: "user", content: "おでん" },
+    ],
+    model: "gpt-4o-mini",
+  });
+
+  const content = completion.choices[0].message.content;
+
+  const response = new Response(content, {
     headers: {
-      "content-type": "text/html;charset=utf-8",
+      "content-type": "test/plain;charset=utf-8",
     },
   });
+
   return response;
 }
 
