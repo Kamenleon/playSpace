9d405458a2fd105024d0d631501aca76a6c1ce76
Kamenleon <leon.kunishi@gmail.com>
2025-05-06 17:52:09 +0900

    add: Section06-11 Done

--- ts/ts-beginner/kanbun/db-index.ts
+++ ts/ts-beginner/kanbun/db-index.ts
@@ -6,45 +6,59 @@ const db = new Database("sqllite.db");
 
 initializeItemTable(db);
 
-if (Bun.argv.length == 4) {
-  // コマンドライン引数の最後の3つの文字列を取得する。
-  const content: string = Bun.argv.pop() ?? "";
-  const command: string = Bun.argv.pop() ?? "";
+try {
+  if (Bun.argv.length == 4) {
+    // コマンドライン引数の最後の3つの文字列を取得する。
+    const content: string = Bun.argv.pop() ?? "";
+    const command: string = Bun.argv.pop() ?? "";
+    
+    switch (command) {
+      case "memo":
+        createItem(db, content, command);
+        break;
+      case "todo":
+        createItem(db, content, command);
+        break;
+      case "done":
+        updateTodoToDone(db, content);
+        break
+      case "drop":
+        deleteItem(db, content);
+        break
+      default:
+        throw new Error("不正なコマンドです");
+    }
+    printItems(db)
+  } else if (Bun.argv.length === 3) {
+    const command: string = Bun.argv.pop() ?? "";
   
-  switch (command) {
-    case "memo":
-      createItem(db, content, command);
-      break;
-    case "todo":
-      createItem(db, content, command);
-      break;
-    case "done":
-      updateTodoToDone(db, content);
-      break
-    case "drop":
-      deleteItem(db, content);
-      break
-    default:
-      throw new Error("不正なコマンドです");
+    switch (command) {
+      case "trim":
+        archiveItems(db);
+        printItems(db);
+        break
+      default:
+        throw new Error("不正なコマンドです");
+    }  
+  } else if (Bun.argv.length === 2) {
+    printItems(db);
+  } else {
+    throw new Error("コマンドライン引数の数が多すぎます");
   }
-  console.log("Done add "+ command);
-} else if (Bun.argv.length === 3) {
-  const command: string = Bun.argv.pop() ?? "";
+  db.close();
+} catch (error) {
+  if (error instanceof Error) {
+    console.error(error.message)
+  }
+}
 
-  switch (command) {
-    case "trim":
-      archiveItems(db);
-      break
-    default:
-      throw new Error("不正なコマンドです");
-  }
-} else if (Bun.argv.length === 2) {
+function printItems(db: Database) {
   const items = getItems(db);
   items.forEach((item) => {
+    if (item.archived) {
+      return;
+    }
     console.log(formatToItem(item));
   });
-} else {
-  throw new Error("コマンドライン引数の数が多すぎます");
 }
-db.close();
 
